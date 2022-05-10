008a4ec63a414cc24ec23d6eab40a5e43f0db7cc
Ruben <rubenoctavio@itsn.edu.mx>
2022-05-09 23:12:32 -0500

    se agregaron todos los archivos al repositorio

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './App.css';
+import Menu from './Menu.js';
+import List from './List.js';
+class App extends React.Component{
+constructor(props){
+    super(props);
+    this.state = {
+      books:[
+        {id:0, rating: 4, title: 'Harry Potter y el cáliz de fuego', image: 'libro01.jpg'},
+        {id:1, rating: 3, title: 'The shining', image: 'libro02.jpg'},
+        {id:2, rating: 5, title: 'Código Da Vinci', image: 'libro03.jpg'},
+        {id:3, rating: 5, title: 'El principito', image: 'libro04.jpg'},
+        {id:4, rating: 5, title: 'Sobrenatural', image: 'libro05.jpg'}
+      ],
+      copyBooks: []
+  };
+   this.onSearch = this.onSearch.bind(this);
+  this.onAdd = this.onAdd.bind(this);
+  }
+   initBooks(){
+    //this.setState({copyBooks: [...this.state.books]});
+    this.setState((state,props) => ({
+      copyBooks: [...state.books]
+    }));
+  }
+   componentDidMount(){
+    this.initBooks();
+  }
+   onSearch(query){
+    if(query === ''){
+      this.setState({copyBooks: [...this.state.books]});
+    }else{
+
+      const temp = [...this.state.books];
+      var res = [];
+      temp.forEach(item =>{
+        if(item.title.toLowerCase().indexOf(query) > -1){
+          res.push(item);
+        }
+      });
+
+      this.setState({copyBooks: [...res]});
+    }
+  }
+  onAdd(item){
+  	console.log(item);
+    var temp = [...this.state.books];
+    const id = temp[temp.length-1].id + 1;
+    item['id'] = id;
+    temp.push(item);
+    this.setState({books: [...temp]});
+    this.initBooks();
+  }
+render(){
+  return (
+    <div className="app">
+    <Menu title="Amozon" onadd={this.onAdd} onsearch={this.onSearch} / >
+    <List items={this.state.copyBooks}/>
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ src/PanelAdd.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class PanelAdd extends React.Component{
+
+    constructor(props){
+        super(props);
+
+       this.state ={
+            title: '',
+            image: '',
+            rating: 1
+        };
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeImage = this.onChangeImage.bind(this);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeRating = this.onChangeRating.bind(this);
+    }
+     onSubmit(e){
+        e.preventDefault();
+        const title = this.state.title;
+        const image = this.state.image;
+        const rating = this.state.rating;
+        this.props.onadd({title: title, image: image, rating: rating});
+        this.props.oncancel();
+    }
+     onChangeTitle(e){
+        this.setState({title: e.target.value});
+
+    }
+    onChangeImage(e){
+        this.setState({image: e.target.value});
+    }
+    onChangeRating(e){
+        const rating = parseInt(e.target.value);
+        this.setState({rating: rating});
+    }
+   render(){
+        return(
+      <div className="new-item-panel-container">
+                <div className="new-item-panel">
+                    <form onSubmit={this.onSubmit}>
+                        <p>
+                        <label>Título del libro</label><br />
+                           <input type="text" name="title" className="input" onChange={this.onChangeTitle} />
+                        </p>
+
+                        <p>
+                        <label>Nombre de imagen</label><br />
+                        <input type="text" name="image" className="input" onChange={this.onChangeImage} />
+                        </p>
+
+                        <p>
+                        <label>Calificación</label><br />
+                         <select onChange={this.onChangeRating}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                        </p>
+                        <input type="submit" className="button btn-blue" value="Registrar libro" />
+                        <button className="button btn-normal" onClick={this.props.oncancel}>Cancelar</button>
+                    </form>
+                </div>
+            </div>
+  );
+}
+}
+export default PanelAdd;
